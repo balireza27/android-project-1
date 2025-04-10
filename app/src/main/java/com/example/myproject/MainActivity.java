6bec0b9413debdb39a9d3515f742e074a8d13eb5
balireza27 <balireza3040@gmail.com>
2025-04-10 14:42:14 +0330

    firs commit

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myproject;
+
+import android.app.assist.AssistStructure;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+
+        });
+
+        EditText edtuser = findViewById(R.id.edituser);
+        EditText edtpass = findViewById(R.id.editpass);
+
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String sUser = edtuser.getText().toString().trim();
+            String sPass = edtpass.getText().toString().trim();
+
+            Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+            intent.putExtra("user",sUser);
+            intent.putExtra("password",sPass);
+            startActivity(intent);
+            }
+        });
+
+
+    }
+}
